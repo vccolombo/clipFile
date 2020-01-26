111341af4848bd30124aec83e1b52acbe131bb6d
Victor Colombo <victorcora98@gmail.com>
2020-01-26 17:06:10 -0300

    Implement copy to clipboard from file

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import sys
+
+import pyperclip
+
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
+
+parser = argparse.ArgumentParser(
+    description="Copy file content to clipboard"
+)
+
+parser.add_argument('File',
+    metavar='file',
+    type=str,
+    help="Path to file to be copied to clipboard"
+)
+
+args = parser.parse_args()
+file_path = args.File
+
+if os.path.isfile(file_path):
+    file = open(file_path, "r")
+
+    file_text = file.read()
+    copy_to_clipboard(file_text)
+else:
+    print(f'{file_path} does not exists or is not a file')
+
